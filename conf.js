17c1afd27b6819cae92b4ea8240fd96fc36dafb8
Rosa Rossi <rosarossig@gmail.com>
2020-08-03 15:46:39 -0400

    qualtrics link

--- conf.js
+++ conf.js
@@ -10,7 +10,5 @@ let links = [];
 let usernameFromParamString = getParamFromURL( 'workerId' );
 links[0]="index.php"+"?workerId="+usernameFromParamString; 
 
-let qualtrics = "https://survey.az1.qualtrics.com/SE/?SID=SV_9uARDX1aXEXq1pP&Q_JFE=0&workerId=";
+let qualtrics = "https://yalesurvey.ca1.qualtrics.com/jfe/form/SV_0U3wW3G3HfY8Ie1?Q_JFE=qdg&workerId=";
 
-// inter-stimulus interval
-let isi = [1000, 5000];
