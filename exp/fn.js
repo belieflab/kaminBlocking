9f463e2b2238721c0ce20bcb0074331791107fb1
Joshua Kenney <joshuagkenney@gmail.com>
2020-10-06 05:58:33 -0400

    adjusted width+= 3.45 to better approximate confidence

--- exp/fn.js
+++ exp/fn.js
@@ -21,7 +21,7 @@ function moveConfidence(data) { // function definition
       jsPsych.finishTrial();  
       // return (event.charCode == 48 || event.charCode == 49)
     } else {
-      width+= 3.34;
+      width+= 3.45; // approximation to reach 100% confidence in 3s
       console.log(width);
       totalConfidence = width;
       trialComplete = 0;
