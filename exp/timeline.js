a3f2d839e2a02dbc6ec38405aebadc754fd1d2fb
joshkenney <joshuagkenney@gmail.com>
2024-01-27 15:06:17 -0500

    updated data_save

--- exp/timeline.js
+++ exp/timeline.js
@@ -241,26 +241,13 @@ let instructions_6 = {
 
 let save_data = {
     type: "html-keyboard-response",
-    stimulus:
-        "<p>Data saving...</p>" +
-        '<div class="sk-cube-grid">' +
-        '<div class="sk-cube sk-cube1"></div>' +
-        '<div class="sk-cube sk-cube2"></div>' +
-        '<div class="sk-cube sk-cube3"></div>' +
-        '<div class="sk-cube sk-cube4"></div>' +
-        '<div class="sk-cube sk-cube5"></div>' +
-        '<div class="sk-cube sk-cube6"></div>' +
-        '<div class="sk-cube sk-cube7"></div>' +
-        '<div class="sk-cube sk-cube8"></div>' +
-        '<div class="sk-cube sk-cube9"></div>' +
-        "</div>" +
-        "<p>Do not close this window until the text dissapears.</p>",
-
+    stimulus: dataSaveAnimation,
     choices: jsPsych.NO_KEYS,
     trial_duration: 5000,
-    on_finish: function () {
-        saveData(taskVersion + "_" + workerId, jsPsych.data.get().csv());
-        document.getElementById("unload").onbeforeunload = "";
+    on_finish: () => {
+        saveData(experimentAlias + "_" + subjectId, jsPsych.data.get().csv()); //function with file name and which type of file as the 2 arguments
+        document.getElementById("unload").onbeforeunload = ""; //removes popup (are you sure you want to exit) since data is saved now
+        // returns cursor functionality
         $(document).ready(function () {
             $("body").addClass("showCursor"); // returns cursor functionality
             closeFullscreen(); // kill fullscreen
