846c869752ad8291f6bdba6ca7bc3d33e02ac83c
joshkenney <joshuagkenney@gmail.com>
2024-05-08 19:25:48 -0400

    refactored moveConfidence()

--- exp/fn.js
+++ exp/fn.js
@@ -1,20 +1,16 @@
 //  increment confidence bar
-const moveConfidence = () => { // function definition
-  let width = document.getElementById("keyBar").style.width; // variable assignment of width property of keyBar
+const moveConfidence = () => {
+    let progressBar = document.getElementById("keyBar");
+    let currentWidth = parseFloat(progressBar.style.width); // Get current width percentage
 
-  width = parseFloat(width.slice(0, -1)); // variable reassignment
-    if (width >= 99) { // set to record 100 taps
-      trialComplete = 1;
-      width = document.getElementById("keyBar").style.width="0%"; // reset to 0
-      jsPsych.finishTrial();  
-      // return (event.charCode == 48 || event.charCode == 49)
+    if (currentWidth >= 99) {
+        trialComplete = 1;
+        progressBar.style.width = "0%"; // Reset progress bar to 0%
+        jsPsych.finishTrial(); // Finish the trial if width reaches 99% or more
     } else {
-      width+= 3.70; // approximation to reach 99.9% confidence in 3s
-      console.log(width);
-      totalConfidence = width;
-      trialComplete = 0;
-      width = document.getElementById("keyBar").style.width=String(width)+"%";
-      // return (event.charCode == 48 || event.charCode == 49)
+        currentWidth += 3.7; // Increment by 3.70% towards a max of 99.9%
+        progressBar.style.width = currentWidth + "%"; // Update the progress bar's width
+        totalConfidence = currentWidth; // Update total confidence level
+        trialComplete = 0;
     }
-
-  }
+};
